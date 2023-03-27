76c99c570c7cae5a5553d1997b8e603761d40ff1
Moe-Thae-Nwe <129031734+Moe-Thae-Nwe@users.noreply.github.com>
2023-03-27 18:38:36 +0630

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let n = document.getElementByID("song")
-let s = document.getElementByID("source")
-const song = []
-const source = []
-let r = Math.floor(Math.random() * song.length)
-n.innerText = song[r]
-s.innerText = source[r]
